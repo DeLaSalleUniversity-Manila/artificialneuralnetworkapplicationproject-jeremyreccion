0fe99c7eb711ef4e01e908e3789ea6d5c5931a18
jeremyreccion <jeremybreccion@yahoo.com>
2015-12-03 18:17:51 +0800

    updated project

---
+++ ANNTesting.m
@@ -0,0 +1,30 @@
+close all;clear;clc
+ 
+%Load the Network file
+load net
+ 
+%Load Testing Data
+inputs = importInput('input.xlsx'); 
+inputs = inputs.data';
+targets = importOutput('output.xlsx'); 
+targets = targets.data';
+ 
+% Test the Network
+outputs = net(inputs);
+errors = gsubtract(targets,outputs);
+performance = perform(net,targets,outputs);
+ 
+% % Recalculate Training, Validation and Test Performance
+% trainTargets = targets .* tr.trainMask{1};
+% valTargets = targets  .* tr.valMask{1};
+% testTargets = targets  .* tr.testMask{1};
+% trainPerformance = perform(net,trainTargets,outputs)
+% valPerformance = perform(net,valTargets,outputs)
+% testPerformance = perform(net,testTargets,outputs)
+ 
+% Plots
+% Uncomment these lines to enable various plots.
+figure, plottrainstate(tr)
+figure, plotperform(tr)
+figure, plotconfusion(targets,outputs)
+figure, ploterrhist(errors)
--- ANNTraining.m
+++ ANNTraining.m
@@ -20,11 +20,11 @@ net.outputs{2}.processFcns = {'removeconstantrows','mapminmax'};
 
 % Setup Division of Data for Training, Validation, Testing
 % For a list of all data division functions type: help nndivide
-net.divideFcn = 'dividerand';  % Divide data randomly
-net.divideMode = 'sample';  % Divide up every sample
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
+% net.divideFcn = 'dividerand';  % Divide data randomly
+% net.divideMode = 'sample';  % Divide up every sample
+net.divideParam.trainRatio = 100/100;
+net.divideParam.valRatio = 100/100;
+net.divideParam.testRatio = 100/100;
 
 % For help on training function 'trainscg' type: help trainscg
 % For a list of all training functions type: help nntrain
@@ -39,31 +39,10 @@ net.performFcn = 'mse';  % Mean squared error
 net.plotFcns = {'plotperform','plottrainstate','ploterrhist', ...
   'plotregression', 'plotfit'};
 
-
 % Train the Network
 [net,tr] = train(net,inputs,targets);
 
-% Test the Network
-outputs = net(inputs);
-errors = gsubtract(targets,outputs);
-performance = perform(net,targets,outputs)
 
-% Recalculate Training, Validation and Test Performance
-trainTargets = targets .* tr.trainMask{1};
-valTargets = targets  .* tr.valMask{1};
-testTargets = targets  .* tr.testMask{1};
-trainPerformance = perform(net,trainTargets,outputs)
-valPerformance = perform(net,valTargets,outputs)
-testPerformance = perform(net,testTargets,outputs)
+%Save the Network to a File
+save net;
 
-% View the Network
-%view(net)
-
-% Plots
-% Uncomment these lines to enable various plots.
-figure, plotperform(tr)
-figure, plottrainstate(tr)
-figure, plotconfusion(targets,outputs)
-figure, plotroc(targets,outputs)
-figure, ploterrhist(errors)
-figure, plotregression(targets,outputs)
